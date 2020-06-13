84d9ab4dc9a5613a36cd93b09557179d5c936c34
Andrew Martin <andy123@stanford.edu>
2020-06-13 11:13:46 -0700

    changed some text from delete to lock

--- project_mass_lock.php
+++ project_mass_lock.php
@@ -1,16 +1,17 @@
 <?php
+namespace Stanford\MassLock;
 
-$massLock = $module;
+$massLock = $module; /** @var MassLock $massLock */
 
 require_once \ExternalModules\ExternalModules::getProjectHeaderPath();
 
 // Verify user rights
-$massLock->validateUserRights('record_delete');
+$massLock->validateUserRights('lock_record');
 $massLock->determineRecords();
 $massLock->handlePost();
 
 // Render Page
-renderPageTitle("<img src='".APP_PATH_IMAGES."application_view_icons.png' class='imgfix2'>&nbsp;Mass Delete a bunch of records");
+renderPageTitle("<img src='".APP_PATH_IMAGES."application_view_icons.png' class='imgfix2'>&nbsp;Mass Lock a bunch of records");
 
 ?>
 <div class="container">
@@ -26,15 +27,14 @@ renderPageTitle("<img src='".APP_PATH_IMAGES."application_view_icons.png' class=
 
     <div class="row">
         <p>
-            This module is used to delete a large number of records.  <strong>Do take caution, it REALLY deletes the records</strong>
-            (but only after a confirmation step).
+            This module is used to lock a large number of records.
         </p>
     </div>
     <div class="row">
-        <form class="delete_records" method='POST'>
+        <form class="lock_records" method='POST'>
             <div class="card">
                 <div class="card-header">
-                    <h6>Select Records for Deletion</h6>
+                    <h6>Select Records for Locking</h6>
 					<?php if (!empty($massLock->arm_options)) { ?>
                         <div>
                             <label for="arms">Arm:&nbsp;</label><select name="arm"><?php echo implode('',$massLock->arm_options) ?></select>
@@ -50,11 +50,11 @@ renderPageTitle("<img src='".APP_PATH_IMAGES."application_view_icons.png' class=
                     <div data-choice="all"    class="btn btn-sm btn-secondary sel"/>All</div>
                     <div data-choice="none"   class="btn btn-sm btn-secondary sel"/>None</div>
                     <div data-choice="custom" class="btn btn-sm btn-secondary customList"/>Custom List</div>
-                    <div id="delete" data-choice='delete' class="btn pull-right btn-danger"><i class="far fa-trash-alt"></i> Delete Selected Records</div>
+                    <div id="lock" data-choice='lock' class="btn pull-right btn-danger"><i class="far fa-trash-alt"></i> Lock Selected Records</div>
                 </div>
             </div>
         </form>
-    <!--</div>   name="Run" value="Delete Selected Records" -->
+    <!--</div>   name="Run" value="Lock Selected Records" -->
     </div>
 </div>
 <?php
@@ -70,8 +70,8 @@ require_once \ExternalModules\ExternalModules::getProjectFooterPath();
     .wrapper br {clear:left;}
     .wrapper {margin-bottom: 1em;}
     .cr {width: 100%; height: 200px; overflow:auto;}
-    .delete_btn { background-color: red; }
-    .delete_btn:hover { background-color: red; background-image: none; }
+    .lock_btn { background-color: red; }
+    .lock_btn:hover { background-color: red; background-image: none; }
 </style>
 <script type='text/javascript'>
     $(document).ready( function() {
@@ -81,18 +81,18 @@ require_once \ExternalModules\ExternalModules::getProjectFooterPath();
             return false;
         });
 
-        $('#delete').click( function() {
+        $('#lock').click( function() {
             var num_selected = $('input[name="records[]"]:checked').length;
             if (num_selected == 0) {
                 simpleDialog('<b>You must first at least one record</b>');
                 return false;
             }
 
-            initDialog("confirmDeletion");
-            $('#confirmDeletion')
-                .html("Are you sure you want to delete the " + num_selected + " selected records!  This is permanent")
+            initDialog("confirmLock");
+            $('#confirmLock')
+                .html("Are you sure you want to lock the " + num_selected + " selected records!")
                 .dialog({
-                    title: "Confirm Deletion",
+                    title: "Confirm Lock",
                     bgiframe:true,
                     modal:true,
                     width:550,
@@ -100,18 +100,18 @@ require_once \ExternalModules\ExternalModules::getProjectFooterPath();
                     open: function(){ fitDialog(this) },
                     buttons: {
                         'Cancel': function() { $(this).dialog('destroy'); },
-                        'Delete': function() {
+                        'Lock': function() {
                             $(this).dialog('destroy');
                             showProgress(1);
                             var input = $("<input>")
                                 .attr("type", "hidden")
-                                .attr("name", "delete").val("true");
-                            $('form.delete_records').append(input).submit();
+                                .attr("name", "lock").val("true");
+                            $('form.lock_records').append(input).submit();
                         }
                     },
                     create:function () {
                         var b = $(this).closest(".ui-dialog")
-                            .find(".ui-dialog-buttonset .ui-button:last").addClass("delete_btn");
+                            .find(".ui-dialog-buttonset .ui-button:last").addClass("lock_btn");
                     }
                 });
 
